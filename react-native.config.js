cd83e701e222dc3c2ebd9a11233d915379d6c84a
wuyuanwuhui99 <275018723@qq.com>
2023-11-18 14:38:37 +0800

    修改配置文件

---
+++ react-native.config.js
@@ -0,0 +1,7 @@
+// 参见文档https://blog.csdn.net/KK_vicent/article/details/129684116
+module.exports = {
+    assets: [
+        'node_modules/@ant-design/icons-react-native/fonts',
+        'node_modules/react-native-vector-icons/fonts',
+    ]
+};
